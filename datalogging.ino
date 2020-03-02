6a360bc696663b7d51bacb44ddcfa871d89deb36
Chester Tan <chestaatan@users.noreply.github.com>
2020-03-02 19:16:33 +0800

    Initial commit

---
+++ datalogging.ino
@@ -0,0 +1,19 @@
+int analogPin = A4  ; // potentiometer wiper (middle terminal) connected to analog pin 3
+                    // outside leads to ground and +5V
+int val = 0;  // variable to store the value read
+float temp;
+
+
+void setup() {
+  Serial.begin(9600);           //  setup serial
+}
+
+void loop() {
+  val = analogRead(analogPin);  // read the input pin
+
+  temp = map(val, 0, 1023, -20.0, 80.0);
+  Serial.print("Temperature = ");
+  Serial.println(temp);
+    
+  delay(100);
+}
